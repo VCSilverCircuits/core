7b590648f32699a86505c0930330dec8781256aa
Dallin Guisti <dallin@guisti.org>
2025-04-17 17:18:31 -0500

    Lv3 hang

--- abstracts/templates/rotator/RotatorActuator.java
+++ abstracts/templates/rotator/RotatorActuator.java
@@ -9,6 +9,7 @@ import vcsc.core.abstracts.state.State;
 public class RotatorActuator<S extends RotatorState<S, P>, P extends RotatorPose> extends Actuator<S> {
     protected ServoImplEx servo;
     protected double angle;
+    protected boolean hasPosition = false;
 
     public RotatorActuator(HardwareMap hardwareMap, String name) {
         servo = hardwareMap.get(ServoImplEx.class, name);
@@ -18,7 +19,9 @@ public class RotatorActuator<S extends RotatorState<S, P>, P extends RotatorPose
     public void loop() {
 //        double rotPosition = angle; // Convert angle to a value between 0 and 1 for the servo
 //        if (angle != servo.getPosition()) {
-        servo.setPosition(angle);
+        if (hasPosition) {
+            servo.setPosition(angle);
+        }
 //        }
     }
 
@@ -26,6 +29,7 @@ public class RotatorActuator<S extends RotatorState<S, P>, P extends RotatorPose
     public void updateState(State<S> newState) {
         RotatorState<S, P> wristState = (RotatorState<S, P>) newState;
         angle = wristState.getAngle();
+        hasPosition = true;
     }
 
 }
